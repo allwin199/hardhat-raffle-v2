048381b50319a8d55fe8f585a370ca9372103f32
worksofallwin <princeallwin199@gmail.com>
2023-07-21 22:22:47 +0530

    Complete requestRandomWinner fn

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,17 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract Raffle {
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughETHEntered();
 
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
-    event RaffleEnter(address indexed player);
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
-    constructor(uint256 _entranceFee) {
+    //VRFCoordinator variables
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1; // only one random no is required
+
+    event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+
+    constructor(
+        uint256 _entranceFee,
+        address _vrfCoordinatorV2,
+        bytes32 _gasLane,
+        uint64 _subscriptionId,
+        uint32 _callbackGasLimit
+    ) VRFConsumerBaseV2(_vrfCoordinatorV2) {
         i_entranceFee = _entranceFee;
+        // vrf
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorV2);
+        i_gasLane = _gasLane;
+        i_subscriptionId = _subscriptionId;
+        i_callbackGasLimit = _callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -20,8 +44,33 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner()
+    // this fn will be called be called automatically by chainlink keepers
+    function requestRandomWinner() external {
+        // to pick a random winner
+        // 1. We need to get random number from chainlink VRF
+        // - chainlink VRF is a 2 transaction process
+        // - If it is done through 1 transaction it can be manipulated
+        // - The current function we are in, will request for RandomNumber
+        // - Then chainlink keepers will call `fullfillRandomnumbers` fn
+        // 2. Then based on the random number, pick the winner.
 
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gasLane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        // requestRandomWords will return a uint256 requestId based on who is requesting
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {}
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
